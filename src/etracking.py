08a36dd99703e19dafeabc19d3621a96d9598225
Ryan.Y.C.Su <Ryan.Y.C.Su@gmail.com>
2024-09-02 14:13:20 +0800

    Refactor etracking.py to class-based structure

--- src/etracking.py
+++ src/etracking.py
@@ -1,151 +1,132 @@
+import io
 import json
 import re
+import logging
 
 import pytesseract
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from PIL import Image
 
-url_base = "https://eservice.7-11.com.tw/e-tracking/"
-url_search = url_base + "search.aspx"
 
+class Etracking:
+    url = "https://eservice.7-11.com.tw/e-tracking/"
+    url_search = url + "search.aspx"
 
-def get_validate_image(url, session):
-    resp = session.get(url)
+    def __init__(self):
+        self.session = requests.Session()
 
-    if resp.status_code != 200:
-        resp.raise_for_status()
+    def search(self, package_id: str):
+        if not self._is_valid_package_id(package_id):
+            return None
 
-    soup = BeautifulSoup(resp.text, "html.parser")
+        response = self.session.get(self.url_search)
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    __VIEWSTATE = soup.find("input", id="__VIEWSTATE").get("value")
-    __VIEWSTATEGENERATOR = soup.find("input", id="__VIEWSTATEGENERATOR").get("value")
+        view_state = self._get_payload_value(soup, "__VIEWSTATE")
+        view_state_generator = self._get_payload_value(soup, "__VIEWSTATEGENERATOR")
+        validate_image = self._get_validate_image(response.text)
+        code = self._get_code(validate_image)
 
-    # Since BeautifulSoup will skip the first src=, so using re instead
-    ValidateImage_url = url_base + re.search(
-        'src="(ValidateImage\.aspx\?ts=[0-9]+)"', resp.text
-    ).group(1)
-    resp = session.get(ValidateImage_url)
+        payload = {
+            "__EVENTTARGET": "submit",
+            "__EVENTARGUMENT": "",
+            "__VIEWSTATE": view_state,
+            "__VIEWSTATEGENERATOR": view_state_generator,
+            "txtProductNum": package_id,
+            "tbChkCode": code,
+            "txtIMGName": "",
+            "txtPage": 1,
+        }
 
-    if resp.status_code != 200:
-        resp.raise_for_status()
-    else:
-        with open("code.jpeg", "wb") as f:
-            f.write(resp.content)
+        response = self.session.post(self.url_search, data=payload)
 
-    return __VIEWSTATE, __VIEWSTATEGENERATOR
+        if response.status_code != 200:
+            return None
 
+        return self.parse_response(response.text)
 
-def get_code(OCR):
-    # if OCR is False, then input the validation code manually
-    if OCR == False:
-        code = input("請輸入驗證碼: ")
-    else:
+    def _is_valid_package_id(self, package_id: str):
+        return len(package_id) == 8 or len(package_id) == 11 or len(package_id) == 12
+
+    def _get_payload_value(self, soup: BeautifulSoup, key: str):
+        tag = soup.find("input", id=key)
+        if isinstance(tag, Tag):
+            return tag.get("value")
+        else:
+            logging.error(f"Failed to get {key}")
+
+    def _get_validate_image(self, html) -> Image.Image:
+        """Get validate image from 7-11 e-tracking website"""
+        validate_image_url = ""
+        url_suffix = re.search(r'src="(ValidateImage\.aspx\?ts=[0-9]+)"', html)
+        if url_suffix is not None:
+            validate_image_url = self.url + url_suffix.group(1)
+
+        try:
+            response = self.session.get(validate_image_url)
+        except requests.exceptions.RequestException:
+            raise Exception("Failed to get validate image")
+
+        return Image.open(io.BytesIO(response.content))
+
+    def _get_code(self, image: Image.Image) -> str:
         tesseract_config = "-c tessedit_char_whitelist=0123456789 --psm 8"
-        code = pytesseract.image_to_string(
-            Image.open("code.jpeg"), config=tesseract_config
-        ).strip()
-    return code
+        code = pytesseract.image_to_string(image, config=tesseract_config).strip()
+        return code
 
+    def parse_response(self, html: str):
+        json_data = {
+            "msg": "",
+            "m_news": "",
+            "result": {"info": {}, "shipping": {"timeline": []}},
+        }
+        soup = BeautifulSoup(html, "html.parser")
 
-def post_search(url, __VIEWSTATE, __VIEWSTATEGENERATOR, txtProductNum, code, session):
-    # Construct the payload which will be used in the POST request
-    payload = {
-        "__EVENTTARGET": "submit",
-        "__EVENTARGUMENT": "",
-        "__VIEWSTATE": __VIEWSTATE,
-        "__VIEWSTATEGENERATOR": __VIEWSTATEGENERATOR,
-        "txtProductNum": txtProductNum,
-        "tbChkCode": code,
-        "txtIMGName": "",
-        "txtPage": 1,
-    }
-    resp = session.post(url, data=payload)
+        # check if there is any alert message
+        script = soup.find_all("script")
+        for i in script:
+            if "alert(" in i.get_text():
+                return {"msg": i.get_text().split("alert('")[1].split("');")[0]}
 
-    if resp.status_code != 200:
-        resp.raise_for_status()
-    return resp.text
+        # check if there is any error message
+        lbmsg = soup.find("span", id="lbMsg")
+        if lbmsg is not None and lbmsg.get_text() != "":
+            return {"msg": lbmsg.get_text()}
 
+        content_result = soup.find("div", id="content_result")
+        if content_result is None:
+            return {"msg": "查無資料"}
 
-def parse_result(html: str):
-    json_data = {
-        "msg": "",
-        "m_news": "",
-        "result": {"info": {}, "shipping": {"timeline": []}},
-    }
-    soup = BeautifulSoup(html, "html.parser")
+        # m_news
+        json_data["m_news"] = content_result.find("div", {"class": "m_news"}).get_text()
+        # json_data["m_news"] = content_result.find("div", class_="m_news").get_text()
 
-    # check if there is any alert message
-    script = soup.find_all("script")
-    for i in script:
-        if "alert(" in i.get_text():
-            return {"msg": i.get_text().split("alert('")[1].split("');")[0]}
+        # result
+        result = content_result.find("div", class_="result")
 
-    # check if there is any error message
-    lbmsg = soup.find("span", id="lbMsg")
-    if lbmsg != None and lbmsg.get_text() != "":
-        return {"msg": lbmsg.get_text()}
+        # info
+        info = result.find("div", class_="info")
+        info_list = info.find_all("span")
+        for i in info_list:
+            json_data["result"]["info"][i.get("id")] = i.get_text()
+        json_data["result"]["info"]["servicetype"] = info.find(
+            "h4", id="servicetype"
+        ).get_text()
 
-    content_result = soup.find("div", id="content_result")
-    if content_result == None:
-        return {"msg": "查無資料"}
+        # shipping
+        shipping = result.find("div", class_="shipping")
+        shipping_list = shipping.find_all("p")
+        for i in shipping_list:
+            json_data["result"]["shipping"]["timeline"].append(i.get_text())
 
-    # m_news
-    json_data["m_news"] = content_result.find("div", class_="m_news").get_text()
+        json_data["msg"] = "success"
 
-    # result
-    result = content_result.find("div", class_="result")
-
-    # info
-    info = result.find("div", class_="info")
-    info_list = info.find_all("span")
-    for i in info_list:
-        json_data["result"]["info"][i.get("id")] = i.get_text()
-    json_data["result"]["info"]["servicetype"] = info.find(
-        "h4", id="servicetype"
-    ).get_text()
-
-    # shipping
-    shipping = result.find("div", class_="shipping")
-    shipping_list = shipping.find_all("p")
-    for i in shipping_list:
-        json_data["result"]["shipping"]["timeline"].append(i.get_text())
-
-    json_data["msg"] = "success"
-
-    return json_data
-
-
-def etracking(txtProductNum: str, OCR: bool = True) -> dict:
-    """
-    7-11 e-tracking
-    :param txtProductNum: The tracking number of 7-11
-    :param OCR: Whether to use OCR to recognize the validation code
-    """
-    session = requests.Session()
-
-    __VIEWSTATE, __VIEWSTATEGENERATOR = get_validate_image(url_search, session)
-    code = get_code(OCR)
-    html = post_search(
-        url_search, __VIEWSTATE, __VIEWSTATEGENERATOR, txtProductNum, code, session
-    )
-    json_data = parse_result(html)
-
-    while json_data["msg"] == "驗證碼錯誤!!":
-        __VIEWSTATE, __VIEWSTATEGENERATOR = get_validate_image(url_search, session)
-        code = get_code(OCR)
-        html = post_search(
-            url_search, __VIEWSTATE, __VIEWSTATEGENERATOR, txtProductNum, code, session
-        )
-        json_data = parse_result(html)
-
-    return json_data
+        return json_data
 
 
 if __name__ == "__main__":
-    res = etracking("87717609642")
+    res = Etracking().search("87717609641")
     print(json.dumps(res, indent=4, ensure_ascii=False))
-    res = etracking("87717609641")
-    print(json.dumps(res, indent=4, ensure_ascii=False))
-    res = etracking("87717609641567")
+    res = Etracking().search("H13804177658")
     print(json.dumps(res, indent=4, ensure_ascii=False))

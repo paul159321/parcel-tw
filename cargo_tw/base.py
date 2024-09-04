ebc81aa173f077163810e1b090839e7021922ed7
Ryan.Y.C.Su <Ryan.Y.C.Su@gmail.com>
2024-09-04 19:57:33 +0800

    Add TrackingInfo dataclass & ABC for Tracker

---
+++ cargo_tw/base.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+
+@dataclass
+class TrackingInfo:
+    order_id: str
+    platform: str
+    status: str
+    time: str | None
+    is_arrived: bool
+    raw_data: dict
+
+
+class Tracker(ABC):
+    @abstractmethod
+    def search(self, order_id: str) -> TrackingInfo | None:
+        pass

217fe63ba7bb38699a381a78f83808cca217e87c
Ryan.Y.C.Su <Ryan.Y.C.Su@gmail.com>
2024-09-06 23:55:26 +0800

    Rename track.py to core.py

--- parcel_tw/__init__.py
+++ parcel_tw/__init__.py
@@ -1,5 +1,5 @@
+from .core import track
 from .enums import Platform
 from .family_mart import FamilyMartTracker
 from .okmart import OKMartTracker
 from .seven_eleven import SevenElevenTracker
-from .track import track

28be2cffd297ccf9805dae1722bfe21321012cba
Ryan.Y.C.Su <Ryan.Y.C.Su@gmail.com>
2024-09-07 21:40:36 +0800

    Refactor OKMartTracker for maintainability

--- parcel_tw/okmart.py
+++ parcel_tw/okmart.py
@@ -7,36 +7,67 @@ from bs4 import BeautifulSoup
 
 from .base import Tracker, TrackingInfo
 
+VALIDATE_URL: Final = "https://ecservice.okmart.com.tw/Tracking/ValidateNumber.ashx"
+RESULT_URL: Final = "https://ecservice.okmart.com.tw/Tracking/Result"
 
 class OKMartTracker(Tracker):
-    VALIDATE_URL: Final = "https://ecservice.okmart.com.tw/Tracking/ValidateNumber.ashx"
-    RESULT_URL: Final = "https://ecservice.okmart.com.tw/Tracking/Result"
-
     def __init__(self) -> None:
         self.session = requests.Session()
         self.tracking_info = None
 
     def track_status(self, order_id: str) -> TrackingInfo | None:
-        logging.info("[OKMart] Getting validate code")
-        validate_code = self._get_validate_code()
-
-        if validate_code is None:
-            logging.error("[OKMart] Failed to get validate code")
+        try:
+            data = OKMartRequestHandler().get_data(order_id)
+        except Exception as e:
+            logging.error(f"[OKMart] {e}")
             return None
 
-        logging.info("[OKMart] Getting the search result")
-        response = self._get_search_result(order_id, validate_code)
-
-        logging.info("[OKMart] Parsing the response")
-        parser = OKMartResponseParser(response.text)
-        raw_data = parser.parse()
-
-        self.tracking_info = self._convert_to_tracking_info(raw_data)
+        self.tracking_info = OKMartTrackingInfoAdapter.convert(data)
 
         return self.tracking_info
 
+
+class OKMartRequestHandler:
+    def __init__(self):
+        """
+        Request handler for OKMart website
+
+        Parameters
+        ----------
+        max_retry : int
+            The maximum number of retries when the captcha is incorrect
+        """
+
+        self.session = requests.Session()
+
+    def get_data(self, order_id: str) -> dict:
+        """
+        Get the tracking information froms OKMart website
+
+        Parameters
+        ----------
+        order_id : str
+            The order_id of the parcel
+
+        Returns
+        -------
+        dict | None
+            The tracking information of the parcel in `dict`, or `None` if failed
+        """
+
+        validate_code = self._get_validate_code()
+
+        if validate_code is None:
+            raise RuntimeError("Failed to get validate code")
+
+        response = self._get_search_result(order_id, validate_code)
+        result = OKMartResponseParser(response.text).parse()
+
+        return result
+
     def _get_validate_code(self) -> str | None:
-        response = self.session.get(self.VALIDATE_URL)
+        logging.info("[OKMart] Getting validate code...")
+        response = self.session.get(VALIDATE_URL)
 
         cookie = response.headers["Set-Cookie"]
         matchobj = re.search(r"ValidateNumber=code=(.....); path=/", cookie)
@@ -46,39 +77,40 @@ class OKMartTracker(Tracker):
     def _get_search_result(
         self, order_id: str, validate_code: str
     ) -> requests.Response:
+        logging.info("[OKMart] Getting search result...")
         headers = {
             "Cookie": f"ValidateNumber=code={validate_code}&odno={order_id}&cutknm=&cutktl="
         }
         params = {"inputOdNo": order_id, "inputCode1": validate_code}
 
-        response = self.session.get(self.RESULT_URL, params=params, headers=headers)
+        response = self.session.get(RESULT_URL, params=params, headers=headers)
         return response
-
-    def _convert_to_tracking_info(self, raw_data: dict) -> TrackingInfo | None:
-        if raw_data["odNo"] is None:
-            return None
-
-        order_id = raw_data["odNo"]
-        status = raw_data["status"]
-        # TODO: Check the message of status is arrived
-        is_delivered = raw_data["status"] == "已送達" or raw_data["status"] == "已取貨"
-
-        return TrackingInfo(
-            order_id=order_id,
-            platform="OKMart",
-            time=None,
-            status=status,
-            is_delivered=is_delivered,
-            raw_data=raw_data,
-        )
 
 
 class OKMartResponseParser:
     def __init__(self, html: str) -> None:
+        """
+        Parser for OKMart tracking response
+
+        Parameters
+        ----------
+        html : str
+            The html content of the response
+        """
+
         self.soup = BeautifulSoup(html, "html.parser")
         self.result = {}
 
     def parse(self) -> dict:
+        """
+        Parse the response and extract the information
+
+        Returns
+        -------
+        dict
+            The extracted information
+        """
+
         self.result["triNo"] = self._find_by_class_name("triNo")  # 寄件編號
         self.result["odNo"] = self._find_by_class_name("odNo")  # 訂單編號
         self.result["type"] = self._find_by_class_name("type")  # 類別
@@ -100,3 +132,38 @@ class OKMartResponseParser:
             return tag.text.strip()
         else:
             return None
+
+class OKMartTrackingInfoAdapter:
+    @staticmethod
+    def convert(raw_data: dict) -> TrackingInfo | None:
+        """
+        Convert the raw data to `TrackingInfo` object
+
+        Parameters
+        ----------
+        raw_data : dict | None
+            The raw data from the 7-11 e-tracking website
+
+        Returns
+        -------
+        TrackingInfo | None
+            A `TrackingInfo` object with the status details of the parcel,
+            or `None` if no information is available.
+        """
+
+        if raw_data["odNo"] is None:
+            return None
+
+        order_id = raw_data["odNo"]
+        status = raw_data["status"]
+        # TODO: Check the message of status is arrived
+        is_delivered = raw_data["status"] == "已送達" or raw_data["status"] == "已取貨"
+
+        return TrackingInfo(
+            order_id=order_id,
+            platform="OKMart",
+            time=None,
+            status=status,
+            is_delivered=is_delivered,
+            raw_data=raw_data,
+        )
